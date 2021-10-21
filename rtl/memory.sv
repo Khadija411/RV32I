668d84cccc44db7da334f04b211a0e2d0dd2ee98
Khadija <alishba200000@gmail.com>
2021-10-21 19:47:44 +0500

    designs updated

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -1,34 +1,35 @@
 // memory
-
 module memory#(parameter width=32, parameter addwidth=12)
   (
-    input logic [addwidth-1 : 0] address_read,
+    input logic [addwidth-1 : 0] address,
     input logic clk,
     input logic [31:0] data_in,
-    input logic [addwidth-1 : 0] address,
     input logic str,
     input logic ld,
-    input logic [3:0]byte_masking,
     output logic [31:0] instruction,
     output logic [31:0] data_out
   );
   
   parameter depth=2**addwidth;
   logic [width-1:0]mem[0:depth-1];
-//   instruction memory
-  assign instruction=mem[address_read];
+  logic [1:0] byte_masking;
+  //   instruction memory
+  initial $readmemh("inst.mem",mem, depth/2);
+  assign instruction=mem[address];
 //   data memory
+  assign byte_masking = address[addwidth-1 : addwidth- 2];
   always @(posedge clk) begin
 //   load
     if (ld)
       data_out<=mem[address];
 //    store
     if (str) begin
+      $display("Byte Masking = %0b", byte_masking);
       case(byte_masking)
-        0:mem[address][7:0]<=data_in[7:0];
-        1:mem[address][15:8]<=data_in[15:8];
-        2:mem[address][23:16]<=data_in[23:16];
-        3:mem[address][31:24]<=data_in[31:24];
+        2'b00:mem[address][7:0]<=data_in[7:0];
+        2'b01:mem[address][15:8]<=data_in[15:8];
+        2'b10:mem[address][23:16]<=data_in[23:16];
+        2'b11:mem[address][31:24]<=data_in[31:24];
         default:mem[address]<=data_in;
       endcase
   	end
--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -1,20 +1,41 @@
-// REGISTER FILE
-module reg_file(
-  input logic [4:0]rs1,
-  input logic [4:0]rs2,
-  input logic [4:0]rd,
+// REGISTER FILE   
+module reg_file#(
+parameter DataWidth=32,
+parameter Registers=32,
+parameter AddrRegWidth=5
+)
+(
+  input logic [AddrRegWidth-1:0]rs1,
+  input logic [AddrRegWidth-1:0]rs2,
+  input logic [AddrRegWidth-1:0]rd,
   input logic wen,
-  input logic [31:0]wdata,
+  input logic [DataWidth-1:0]wdata,
   input logic clk,
-  output logic [31:0]rdata1,
-  output logic [31:0]rdata2
+  input logic rst,
+  output logic [DataWidth-1:0]rdata1,
+  output logic [DataWidth-1:0]rdata2
 );
-  logic [31:0]r[0:31];
+  logic [DataWidth-1:0]r[0:Registers-1];
+
+  integer i;
+initial begin
+  for (i=0;i<(Registers);i=i+1)begin
+    r[i]<=0;
+  end
+  r[2]<=32'h00000200;
+end
+
   always_ff @(posedge clk)begin
-    
-    if (wen)begin
-      r[rd]<=wdata;
-    end
+    if (rst) begin
+        for (i=0;i<(Registers);i=i+1)
+            r[i]<=0;
+        end
+    else if (!rst && wen)begin
+            if (rd==5'd0)
+                r[0]<=32'h00000000;
+            else
+                r[rd]<=wdata;       
+        end
     
   end
   assign rdata1=r[rs1];
